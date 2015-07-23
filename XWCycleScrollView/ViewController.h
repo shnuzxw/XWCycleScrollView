d9dc8ece93c1448e14a696e67c07c163373a0003
Zhang Xiaowei <weixiao08@qq.com>
2015-07-23 20:39:58 +0800

    Initial Commit

---
+++ XWCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XWCycleScrollView
+//
+//  Created by Zhang Xiaowei on 15/7/23.
+//  Copyright (c) 2015年 Zhang Xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XWCycleScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XWCycleScrollView
+//
+//  Created by Zhang Xiaowei on 15/7/23.
+//  Copyright (c) 2015年 Zhang Xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
